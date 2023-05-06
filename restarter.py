a20af0fc07901b4e2004c4ae8be879150596d038
Luke3573 <132735214+Luke3573@users.noreply.github.com>
2023-05-06 23:59:05 +0800

    Add files via upload

--- restarter.py
+++ restarter.py
@@ -1,9 +1,9 @@
 import subprocess
 
-filename = 'Desktop/stripper1.py'
+filename = 'WaFuel.py'
 while True:
     """However, you should be careful with the '.wait()'"""
-    p = subprocess.Popen('python3.8 '+filename, shell=True).wait()
+    p = subprocess.Popen('python3 '+filename, shell=True).wait()
 
     """#if your there is an error from running 'monitor/monitor.py',
     the while loop will be repeated,
